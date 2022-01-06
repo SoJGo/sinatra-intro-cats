1f9ba5fd3159393fdbc1d86c703ee13e83a27ae4
SophieGowen <sophie.j.gowen@gmail.com>
2022-01-06 14:20:00 +0000

    Add named-cat route

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 
 get '/' do
-  "Hello World"
+  "Hello World!"
 end
 
 get '/secret' do
@@ -17,6 +17,13 @@ get '/orange' do
   "Blue!"
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name = ['Amigo', 'Misty', 'Almond'].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
